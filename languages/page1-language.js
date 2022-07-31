2b263eb1c137895f094e6fc72386f18dfc02fc51
Jeffrey Tuason <jepoyduque06@gmail.com>
2022-07-31 16:47:00 +0800

    page 1 done

---
+++ languages/page1-language.js
@@ -0,0 +1,397 @@
+
+// Language translations
+const language = {
+    en: {
+        hired: "Hire me",
+
+        optimize: "Optimization of Global Supply Chain",
+        optimizeP1: "OTIF is the next dimension of consolidation, orchestration, and optimization of the global supply chain networks, integrating different production enterprises and logistics providers for your convenience.",
+        optimizeBtn: "start",
+
+        // grid item 1
+        dataDriven: "Data Driven",
+        dataDrivenP1: "We decided to use technology in logistics to habilitate transparent, data-driven, and easy decisions for your business. Our technology is your business’ competitive advantage.",
+        dataDrivenP2: "This brings decisions into the user’s hands. Users choose to their company’s convenience the times, prices, and modalities that they need and want.",
+
+        // grid item 2
+        efficiency: "Efficiency and Transparency",
+        efficiencyP1: "OTIF is a platform that can smoothly and flexibly integrate all parties across multiple supply chains, enabling the formation of modular services for your needs, maximizing your efficiency, and reducing your costs.",
+        
+        // grid item 3
+        ontime: "On-Time In Full",
+        ontimeP1: "Do you ever wish there was a platform where you could search, compare, and buy the best, most reliable logistical bookings?",
+        ontimeP2: "Now there is, OTIF.",
+        ontimeP3: "OTIF: ON-TIME IN FULL",
+        ontimeP4: "Our team is your team.",
+        ontimeP5: "Our technology is your technology to use as you please when you please.",
+        ontimeP6: "The power of your business.",
+
+        // Queries
+        queries: "If you Have any Questions Contact Us",
+        queriesP1: "OTIF bring together all the packages in mexico and the world. otif Products facilitate logistics for your company that operates on the internet or in person.Our services includes",
+        queriesBtn: "Request a Quote",
+
+        // Footer
+        products: "Products",
+        productsLink1: "Terms and Conditions",
+        productsLink2: "Companies",
+        productsLink3: "Quoting",
+        productsLink4: "Security",
+        productsLink5: "Why OTIF?",
+
+        shipping: "Shipping",
+        shippingLink1: "Shipping",
+        shippingLink2: "Shipping to",
+        shippingLink3: "Deliveries to Mexico",
+        shippingLink4: "Cheap shipping",
+        shippingLink5: "Package shipments",
+        shippingLink6: "Economic shipments",
+        shippingLink7: "National shipments",
+        shippingLink8: "Secure shipments",
+        shippingLink9: "Prepaid guides",
+        shippingLink10: "Express courier",
+        shippingLink11: "Economic package",
+        shippingLink12: "Packages in Mexico",
+
+        international: "IInternational Shipping",
+        internationalLink1: "International Deliveries",
+        internationalLink2: "Shipping Monterrey",
+        internationalLink3: "Secure shipments",
+
+        parcels: "Parcels",
+        parcelsLink1: "Mails from Mexico",
+        parcelsLink2: "Carssa",
+        parcelsLink3: "Express Package",
+        parcelsLink4: "Post Office",
+        parcelsLink5: "Redpack",
+        parcelsLink6: "Sendex",
+        parcelsLink7: "UPS",
+        parcelsLink8: "DHL",
+
+        subscribe: "Subscribe to our newsletter",
+        subscribeBtn: "Subscribe",
+
+        footer1: `Copyright &copy; <span id="f-year"></span>. All rights reserved`,
+        footer2: "Privacy & policy",
+        footer3: "Terms & Condition",
+    },
+    tl: {
+        hired: "I-hire mo ako",
+
+        optimize: "Pag-optimize ng Global Supply Chain",
+        optimizeP1: "Ang OTIF ay ang susunod na dimensyon ng pagsasama-sama, orkestrasyon, at pag-optimize ng mga pandaigdigang network ng supply chain, pagsasama-sama ng iba't ibang mga enterprise ng produksyon at mga provider ng logistik para sa iyong kaginhawahan.",
+        optimizeBtn: "simulan",
+
+        // grid item 2
+        dataDriven: "Hinihikayat ng Data",
+        dataDrivenP1: "Napagpasyahan naming gumamit ng teknolohiya sa logistik upang maging malinaw, batay sa data, at madaling mga desisyon para sa iyong negosyo. Ang aming teknolohiya ang competitive advantage ng iyong negosyo.",
+        dataDrivenP2: "Dinadala nito ang mga desisyon sa mga kamay ng gumagamit. Pinipili ng mga user sa kaginhawahan ng kanilang kumpanya ang mga oras, presyo, at modalidad na kailangan at gusto nila.",
+        
+        // grid item 2
+        efficiency: "Kahusayan at Transparency",
+        efficiencyP1: "Ang OTIF ay isang platform na maaaring maayos at may kakayahang umangkop na isama ang lahat ng partido sa maraming supply chain, na nagbibigay-daan sa pagbuo ng mga modular na serbisyo para sa iyong mga pangangailangan, pag-maximize ng iyong kahusayan, at pagbabawas ng iyong mga gastos.",
+        // grid item 3
+        ontime: "Nasa Oras sa Buong",
+        ontimeP1: "Nais mo bang magkaroon ng isang platform kung saan maaari kang maghanap, maghambing, at bumili ng pinakamahusay, pinaka-maaasahang logistical booking?",
+        ontimeP2: "Ngayon meron, OTIF.",
+        ontimeP3: "OTIF : ON-TIME NA BUO",
+        ontimeP4: "Ang aming koponan ay ang iyong koponan.",
+        ontimeP5: "Ang aming teknolohiya ay ang iyong teknolohiya na gagamitin ayon sa gusto mo kapag gusto mo.",
+        ontimeP6: "Ang lakas ng negosyo mo.",
+
+        // Queries
+        queries: "Kung mayroon kang anumang mga Tanong Makipag-ugnayan sa Amin",
+        queriesP1: "Pinagsama-sama ng OTIF ang lahat ng mga pakete sa mexico at sa mundo. Ang mga otif Products ay nagpapadali ng logistik para sa iyong kumpanya na nagpapatakbo sa internet o nang personal. Kasama sa aming mga serbisyo",
+        queriesBtn: "Humiling ng Quote",
+
+        // Footer
+        products: "Mga produkto",
+        productsLink1: "Mga Tuntunin at Kundisyon",
+        productsLink2: "Mga kumpanya",
+        productsLink3: "Sinipi",
+        productsLink4: "Seguridad",
+        productsLink5: "Bakit OTIF?",
+
+        shipping: "Pagpapadala",
+        shippingLink1: "Pagpapadala",
+        shippingLink2: "Ipapadala sa",
+        shippingLink3: "Paghahatid sa Mexico",
+        shippingLink4: "Murang shipping",
+        shippingLink5: "Mga pagpapadala ng package",
+        shippingLink6: "Mga padala sa ekonomiya",
+        shippingLink7: "Pambansang padala",
+        shippingLink8: "Mga ligtas na pagpapadala",
+        shippingLink9: "Mga prepaid na gabay",
+        shippingLink10: "Express courier",
+        shippingLink11: "Pang-ekonomiyang pakete",
+        shippingLink12: "Mga package sa Mexico",
+
+        international: "Internasyonal na Pagpapadala",
+        internationalLink1: "Mga Internasyonal na Paghahatid",
+        internationalLink2: "Pagpapadala ng Monterrey",
+        internationalLink3: "Mga ligtas na pagpapadala",
+
+        parcels: "Mga parsela",
+        parcelsLink1: "Mga sulat mula sa Mexico",
+        parcelsLink1: "Carssa",
+        parcelsLink1: "Express Package",
+        parcelsLink1: "Post Opisina",
+        parcelsLink1: "Redpack",
+        parcelsLink1: "Sendex",
+        parcelsLink1: "UPS",
+        parcelsLink1: "DHL",
+
+        subscribe: "Mag-subscribe sa aming newsletter",
+        subscribeBtn: "Mag-subscribe",
+
+        footer1: `Copyright &copy; <span id="f-year"></span>. Lahat ng karapatan ay nakalaan`,
+        footer2: "Pagkapribado at patakaran",
+        footer3: "Mga Tuntunin at kondisyon",
+    },
+};
+
+// Define language via window hash
+if(window.location.hash) {
+    if(window.location.hash === '#tl') {
+        hired.textContent = language.tl.hired;
+
+        optimize.textContent = language.tl.optimize;
+        optimizeP1.textContent = language.tl.optimizeP1;
+        optimizeBtn.textContent = language.tl.optimizeBtn;
+        
+        // grid item 1
+        dataDriven.textContent = language.tl.dataDriven;
+        dataDrivenP1.textContent = language.tl.dataDrivenP1;
+        dataDrivenP2.textContent = language.tl.dataDrivenP2;
+
+        // grid item 2
+        efficiency.textContent = language.tl.efficiency;
+        efficiencyP1.textContent = language.tl.efficiencyP1;
+        
+        // grid item 3
+        ontime.textContent = language.tl.ontime;
+        ontimeP1.textContent = language.tl.ontimeP1;
+        ontimeP2.textContent = language.tl.ontimeP2;
+        ontimeP3.textContent = language.tl.ontimeP3;
+        ontimeP4.textContent = language.tl.ontimeP4;
+        ontimeP5.textContent = language.tl.ontimeP5;
+        ontimeP6.textContent = language.tl.ontimeP6;
+
+        // Queries
+        queries.textContent = language.tl.queries;
+        queriesP1.textContent = language.tl.queriesP1;
+        queriesBtn.textContent = language.tl.queriesBtn;
+
+        // Footer
+        products.textContent = language.tl.products;
+        productsLink1.textContent = language.tl.productsLink1;
+        productsLink2.textContent = language.tl.productsLink2;
+        productsLink3.textContent = language.tl.productsLink3;
+        productsLink4.textContent = language.tl.productsLink4;
+        productsLink5.textContent = language.tl.productsLink5;
+
+        shipping.textContent = language.tl.shipping;
+        shippingLink1.textContent = language.tl.shippingLink1;
+        shippingLink2.textContent = language.tl.shippingLink2;
+        shippingLink3.textContent = language.tl.shippingLink3;
+        shippingLink4.textContent = language.tl.shippingLink4;
+        shippingLink5.textContent = language.tl.shippingLink5;
+        shippingLink6.textContent = language.tl.shippingLink6;
+        shippingLink7.textContent = language.tl.shippingLink7;
+        shippingLink8.textContent = language.tl.shippingLink8;
+        shippingLink9.textContent = language.tl.shippingLink9;
+        shippingLink10.textContent = language.tl.shippingLink10;
+        shippingLink11.textContent = language.tl.shippingLink11;
+        shippingLink12.textContent = language.tl.shippingLink12;
+
+        international.textContent = language.tl.international;
+        internationalLink1.textContent = language.tl.internationalLink1;
+        internationalLink2.textContent = language.tl.internationalLink2;
+        internationalLink3.textContent = language.tl.internationalLink3;
+
+        parcels.textContent = language.tl.parcels;
+        parcelsLink1.textContent = language.tl.parcelsLink1;
+        parcelsLink2.textContent = language.tl.parcelsLink2;
+        parcelsLink3.textContent = language.tl.parcelsLink3;
+        parcelsLink4.textContent = language.tl.parcelsLink4;
+        parcelsLink5.textContent = language.tl.parcelsLink5;
+        parcelsLink6.textContent = language.tl.parcelsLink6;
+        parcelsLink7.textContent = language.tl.parcelsLink7;
+        parcelsLink8.textContent = language.tl.parcelsLink8;
+
+        subscribe.textContent = language.tl.subscribe;
+        subscribeBtn.textContent = language.tl.subscribeBtn;
+
+        footer1.innerHTML =  language.tl.footer1;
+        footer2.textContent = language.tl.footer2;
+        footer3.textContent = language.tl.footer3;
+    }
+    if(window.location.hash === '#en') {
+        hired.textContent = language.en.hired;
+        
+        optimize.textContent = language.en.optimize;
+        optimizeP1.textContent = language.en.optimizeP1;
+        optimizeBtn.textContent = language.en.optimizeBtn;
+
+        // grid item 1
+        dataDriven.textContent = language.en.dataDriven;
+        dataDrivenP1.textContent = language.en.dataDrivenP1;
+        dataDrivenP2.textContent = language.en.dataDrivenP2;
+
+        // grid item 2
+        efficiency.textContent = language.en.efficiency;
+        efficiencyP1.textContent = language.en.efficiencyP1;
+        
+        // grid item 3
+        ontime.textContent = language.en.ontime;
+        ontimeP1.textContent = language.en.ontimeP1;
+        ontimeP2.textContent = language.en.ontimeP2;
+        ontimeP3.textContent = language.en.ontimeP3;
+        ontimeP4.textContent = language.en.ontimeP4;
+        ontimeP5.textContent = language.en.ontimeP5;
+        ontimeP6.textContent = language.en.ontimeP6;
+
+        // Queries
+        queries.textContent = language.en.queries;
+        queriesP1.textContent = language.en.queriesP1;
+        queriesBtn.textContent = language.en.queriesBtn;
+
+        // Footer
+        products.textContent = language.en.products;
+        productsLink1.textContent = language.en.productsLink1;
+        productsLink2.textContent = language.en.productsLink2;
+        productsLink3.textContent = language.en.productsLink3;
+        productsLink4.textContent = language.en.productsLink4;
+        productsLink5.textContent = language.en.productsLink5;
+
+        shipping.textContent = language.en.shipping;
+        shippingLink1.textContent = language.en.shippingLink1;
+        shippingLink2.textContent = language.en.shippingLink2;
+        shippingLink3.textContent = language.en.shippingLink3;
+        shippingLink4.textContent = language.en.shippingLink4;
+        shippingLink5.textContent = language.en.shippingLink5;
+        shippingLink6.textContent = language.en.shippingLink6;
+        shippingLink7.textContent = language.en.shippingLink7;
+        shippingLink8.textContent = language.en.shippingLink8;
+        shippingLink9.textContent = language.en.shippingLink9;
+        shippingLink10.textContent = language.en.shippingLink10;
+        shippingLink11.textContent = language.en.shippingLink11;
+        shippingLink12.textContent = language.en.shippingLink12;
+
+        international.textContent = language.en.international;
+        internationalLink1.textContent = language.en.internationalLink1;
+        internationalLink2.textContent = language.en.internationalLink2;
+        internationalLink3.textContent = language.en.internationalLink3;
+
+        parcels.textContent = language.en.parcels;
+        parcelsLink1.textContent = language.en.parcelsLink1;
+        parcelsLink2.textContent = language.en.parcelsLink2;
+        parcelsLink3.textContent = language.en.parcelsLink3;
+        parcelsLink4.textContent = language.en.parcelsLink4;
+        parcelsLink5.textContent = language.en.parcelsLink5;
+        parcelsLink6.textContent = language.en.parcelsLink6;
+        parcelsLink7.textContent = language.en.parcelsLink7;
+        parcelsLink8.textContent = language.en.parcelsLink8;
+
+        subscribe.textContent = language.en.subscribe;
+        subscribeBtn.textContent = language.en.subscribeBtn;
+
+        footer1.innerHTML =  language.en.footer1;
+        footer2.textContent = language.en.footer2;
+        footer3.textContent = language.en.footer3;
+    }
+}
+
+// check on load if there is a preference language
+const currentLocation = `${window.location.origin}${window.location.pathname}`;
+
+if(localStorage.getItem('preference') && window.location != `${currentLocation}#${localStorage.getItem('preference')}`) {
+    window.location.replace(`${currentLocation}#${localStorage.getItem('preference')}`);
+    window.location.reload();
+};
+
+// set preference language
+const btns = document.querySelectorAll('[data-translation]');
+btns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+
+        const preference = e.currentTarget.dataset.id;
+        localStorage.setItem('preference', preference);
+
+        window.location.replace(`${currentLocation}#${preference}`);
+        window.location.reload();
+    })
+});
+
+let x = {
+    en: {
+        hired: "",
+        
+        // grid item 1
+        dataDriven: "",
+        dataDrivenP1: "",
+        dataDrivenP2: "",
+
+        // grid item 2
+        efficiency: "",
+        efficiencyP1: "",
+        
+        // grid item 3
+        ontime: "",
+        ontimeP1: "",
+        ontimeP2: "",
+        ontimeP3: "",
+        ontimeP4: "",
+        ontimeP5: "",
+        ontimeP6: "",
+
+        // Queries
+        queries: "",
+        queriesP1: "",
+        queriesBtn: "",
+
+        // Footer
+        products: "",
+        productsLink1: "",
+        productsLink2: "",
+        productsLink3: "",
+        productsLink4: "",
+        productsLink5: "?",
+
+        shipping: "",
+        shippingLink1: "",
+        shippingLink2: "",
+        shippingLink3: "",
+        shippingLink4: "",
+        shippingLink5: "",
+        shippingLink6: "",
+        shippingLink7: "",
+        shippingLink8: "",
+        shippingLink9: "",
+        shippingLink10: "",
+        shippingLink11: "",
+        shippingLink12: "",
+
+        international: "",
+        internationalLink1: "",
+        internationalLink2: "",
+        internationalLink3: "",
+
+        parcels: "",
+        parcelsLink1: "",
+        parcelsLink1: "",
+        parcelsLink1: "",
+        parcelsLink1: "",
+        parcelsLink1: "",
+        parcelsLink1: "",
+        parcelsLink1: "",
+        parcelsLink1: "",
+
+        subscribe: "",
+        subscribeBtn: "",
+
+        footer1: `Copyright &copy; <span id="f-year"></span>. All rights reserved`,
+        footer2: "Privacy & policy",
+        footer3: "Terms & Condition",
+    },
+}
